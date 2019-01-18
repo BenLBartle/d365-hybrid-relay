68babc1f209e41bf18fe71f699d4d06559e2cc29
Ben Bartle <benbartle@bens-mbp.home>
2019-01-18 16:45:58 +0000

    ASP.NET WebApi Project

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspRelay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static int _currentId = 0;
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<int> Get()
+        {
+            return Interlocked.Increment(ref _currentId);
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.Azure.Relay.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Logging;
+
+namespace AspRelay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseAzureRelay(options =>
+                {
+                    options.UrlPrefixes.Add(Environment.GetEnvironmentVariable("RelayConnectionString"));
+                });
+    }
+}
